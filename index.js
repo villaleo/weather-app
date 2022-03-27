07045e795974b53d6af73d8b02df04a6871c5834
Leonardo Villalobos <levillalobos@csumb.edu>
2022-03-27 11:33:47 -0700

    Setup additional routes.

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/how-it-works', (req, res) => {
+    res.render('how_it_works')
+})
+
+app.get('/four-day-forecast', (req, res) => {
+    res.render('four_day_forecast')
+})
+
 app.listen(3000, () => {
-    console.log('server started')
+    console.log('Server started on port 3000.')
 })
