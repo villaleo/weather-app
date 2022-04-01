40da9e7bbbf63ab00391e139f0e1e9c5bf282eb3
Leonardo Villalobos <levillalobos@csumb.edu>
2022-04-01 15:48:48 -0700

    Refactor code.

--- index.js
+++ index.js
@@ -1,54 +1,50 @@
 const express = require('express')
-const { getStateCodeByStateName } = require('us-state-codes')
 const fetch = require('node-fetch')
 const states = require('us-state-codes')
-const geocodingEndpoint = 'http://api.openweathermap.org/geo/1.0/direct?'
+
+const { getStateCodeByStateName } = require('us-state-codes')
+const geocodingEndpoint = 'https://api.openweathermap.org/geo/1.0/direct?'
 const weatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather?'
+const forecastEndpoint = 'https://api.openweathermap.org/data/2.5/forecast?'
 const API_KEY = '9965dce7f97fcb2a4d726496253bda5b' // <-- replace with your own key
 
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('home', {
-        error: false
-    })
-})
+const locationCoordinates = async (city, state) => {
+    let stateCode = getStateCodeByStateName(state)
 
-app.get('/forecast', async (req, res) => {
-    let cityName = req.query.city
-    let stateCode = getStateCodeByStateName(req.query.state)
-    
-    let geocodingArgs = `q=${cityName},${stateCode},US&limit=1&appid=${API_KEY}`
+    let geocodingArgs = `q=${city},${stateCode},US&limit=1&appid=${API_KEY}`
     let geocodingResponse = await fetch(geocodingEndpoint + geocodingArgs)
     let geocodingData = await geocodingResponse.json()
 
-    if (geocodingData?.length === 0) {
-        console.log('No such city found.')
+    return geocodingData?.length === 0 ? { error: true } : {
+        latitude: geocodingData[0]?.lat,
+        longitude: geocodingData[0]?.lon
+    }
+}
+
+app.get('/', (req, res) => {
+    res.render('home', { error: false })
+})
+
+app.get('/forecast', async (req, res) => {
+    let coordinates = await locationCoordinates(req.query.city, req.query.state)
+    if (coordinates.error) {
         return res.render('home', {
             error: true,
             message: 'No such city found.'
         })
     }
-    else if (geocodingData.cod) {
-        console.log('No parameters specified.')
-        return res.render('home', {
-            error: true,
-            message: 'No parameters specified.'
-        })
-    }
 
-    let latitude = geocodingData[0]?.lat
-    let longitude = geocodingData[0]?.lon
-
+    let { latitude, longitude } = coordinates
     let weatherArgs = `lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=imperial`
+
     let weatherResponse = await fetch(weatherEndpoint + weatherArgs)
     let weatherData = await weatherResponse.json()
 
-    res.render('forecast', {
-        data: weatherData
-    })
+    res.render('forecast', { data: weatherData })
 })
 
 app.get('/how-it-works', (req, res) => {
@@ -56,40 +52,21 @@ app.get('/how-it-works', (req, res) => {
 })
 
 app.get('/weekly-forecast', async (req, res) => {
-    let cityName = req.query.city
-    let stateCode = getStateCodeByStateName(req.query.state)
-
-    let geocodingArgs = `q=${cityName},${stateCode},US&limit=1&appid=${API_KEY}`
-    let geocodingResponse = await fetch(geocodingEndpoint + geocodingArgs)
-    let geocodingData = await geocodingResponse.json()
-
-    if (geocodingData?.length === 0) {
-        console.log('No such city found.')
+    let coordinates = await locationCoordinates(req.query.city, req.query.state)
+    if (coordinates.error) {
         return res.render('home', {
             error: true,
             message: 'No such city found.'
         })
     }
-    else if (geocodingData.cod) {
-        console.log('No parameters specified.')
-        return res.render('home', {
-            error: true,
-            message: 'No parameters specified.'
-        })
-    }
 
-    let latitude = geocodingData[0]?.lat
-    let longitude = geocodingData[0]?.lon
-
-    const forecastEndpoint = 'https://api.openweathermap.org/data/2.5/forecast?'
-
+    let { latitude, longitude } = coordinates;
     let args = `lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=imperial`
 
     let forecastResponse = await fetch(forecastEndpoint + args)
     let forecastData = await forecastResponse.json()
 
     let days = { '1': [], '2': [], '3': [], '4': [], '5': [] }
-
     let position = 0
     for (let i = 0; i < forecastData.list.length; i++) {
         if (i % 8 === 0) {
